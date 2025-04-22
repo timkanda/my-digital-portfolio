4261302a510def5ccb2c9826019b27caae889c3a
mohdz <rushhinjam@gmail.com>
2025-04-23 00:18:49 +0530

    added the route protect for unsigned users and the for resources

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,6 @@
 import { clerkMiddleware,createRouteMatcher } from "@clerk/nextjs/server";
 
-const isProtectedRoute = createRouteMatcher(['/admin'])
+const isProtectedRoute = createRouteMatcher(['/admin','/blog','/resources(.*)','/projects'])
 
 export default clerkMiddleware(async (auth, req) => {
   if (isProtectedRoute(req)) await auth.protect()
